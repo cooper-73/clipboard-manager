43fd6ac8e7528e426eb1ae28c1f3507711f1027c
Cristhian Cruz <c.cruzcoro73@gmail.com>
2026-02-20 12:52:31 -0500

    feat: Add scroll bar to clipboard history list

--- lib/presentation/widgets/clipboard_history.dart
+++ lib/presentation/widgets/clipboard_history.dart
@@ -3,7 +3,7 @@ import 'package:clipboard_manager/domain/domain.dart';
 import 'package:clipboard_manager/presentation/presentation.dart';
 import 'package:flutter/material.dart';
 
-class ClipboardHistory extends StatelessWidget {
+class ClipboardHistory extends StatefulWidget {
   const ClipboardHistory({
     super.key,
     required this.clipboardItems,
@@ -12,32 +12,58 @@ class ClipboardHistory extends StatelessWidget {
   final List<ClipboardItem> clipboardItems;
 
   @override
+  State<ClipboardHistory> createState() => _ClipboardHistoryState();
+}
+
+class _ClipboardHistoryState extends State<ClipboardHistory> {
+  late ScrollController _scrollController;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController = ScrollController();
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return ListView.separated(
-      padding: const EdgeInsets.symmetric(
-        horizontal: AppSpacing.sm,
-        vertical: AppSpacing.md,
+    return Scrollbar(
+      controller: _scrollController,
+      thumbVisibility: true,
+      trackVisibility: true,
+      interactive: true,
+      child: ListView.separated(
+        controller: _scrollController,
+        padding: const EdgeInsets.symmetric(
+          horizontal: AppSpacing.sm,
+          vertical: AppSpacing.md,
+        ),
+        itemCount: widget.clipboardItems.length,
+        findChildIndexCallback: (key) {
+          final valueKey = key as ValueKey<int>;
+
+          final index = widget.clipboardItems.indexWhere(
+            (item) => item.createdAt.millisecondsSinceEpoch == valueKey.value,
+          );
+
+          return index != -1 ? 2 * index : null;
+        },
+        separatorBuilder: (context, index) =>
+            const SizedBox(height: AppSpacing.xxs),
+        itemBuilder: (context, index) {
+          final clipboardItem = widget.clipboardItems[index];
+
+          return ClipboardEntryCard(
+            key: ValueKey(clipboardItem.createdAt.millisecondsSinceEpoch),
+            clipboardItem: clipboardItem,
+          );
+        },
       ),
-      itemCount: clipboardItems.length,
-      findChildIndexCallback: (key) {
-        final valueKey = key as ValueKey<int>;
-
-        final index = clipboardItems.indexWhere(
-          (item) => item.createdAt.millisecondsSinceEpoch == valueKey.value,
-        );
-
-        return index != -1 ? 2 * index : null;
-      },
-      separatorBuilder: (context, index) =>
-          const SizedBox(height: AppSpacing.xxs),
-      itemBuilder: (context, index) {
-        final clipboardItem = clipboardItems[index];
-
-        return ClipboardEntryCard(
-          key: ValueKey(clipboardItem.createdAt.millisecondsSinceEpoch),
-          clipboardItem: clipboardItem,
-        );
-      },
     );
   }
 }
